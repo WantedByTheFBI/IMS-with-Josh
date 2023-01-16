aa1d2c014265c87ff8367ba071791d097e80343e
jackgood <jackmichaelgood@gmail.com>
2023-01-16 10:37:22 -0500

    cleaned code up

--- client.py
+++ client.py
@@ -1,16 +1,15 @@
 import socket
-host = 
-port = 9443
+host = "Lab120-04"
+port = 9440
 buffer_size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 while True:
-    text = ("https://github.com/WantedByTheFBI/IMS-with-Josh")
-    text = text.encode('utf-8')
-    s.send(text)
-    breakoff = input("would you like to stop sending and receiving messages? (yes or no): ")
-    if breakoff.lower() == "yes":
+    text = input("What would you like to send?(or enter [close] if you want to stop messaging: ").encode("utf-8")
+    if text.decode() == "[close]":
+        s.send(("The client has closed the application.").encode("utf-8"))
         break
+    s.send(text)
     data = s.recv(buffer_size)
     print("received data:", data)
 s.close()
--- server.py
+++ server.py
@@ -2,17 +2,16 @@ import socket
 mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 buffer_size = 1024
 text = "no"
-mysocket.bind(('Lab120-04', 9440))
+mysocket.bind((socket.gethostname(), 9440))
 mysocket.listen()
 (client, (ip,port)) = mysocket.accept()
 print(client, port)
 client.send(b"knock knock knock, I'm the server")
 while True:
-    message = input("What would you like to send? ").encode("utf-8")
+    message = input("What would you like to send?(or enter [close] if you want to stop messaging: ").encode("utf-8")
+    if message.decode() == "[close]":
+        break
     client.send(message)
     data = client.recv(buffer_size)
-    print(data.decode())
-    breakoff = input("would you like to stop sending and receiving messages? (yes or no): ")
-    if breakoff.lower() == "yes":
-        break
+    print("received data:")
 mysocket.close()
