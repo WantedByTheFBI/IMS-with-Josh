1f12d865d15eb70e2f5807e72357b5d90b847be8
jackgood <jackmichaelgood@gmail.com>
2023-01-17 13:55:39 -0500

    new threading code

--- server.py
+++ server.py
@@ -5,13 +5,13 @@ text = "no"
 mysocket.bind((socket.gethostname(), 9440))
 mysocket.listen()
 (client, (ip,port)) = mysocket.accept()
-print(client, port)
-client.send(b"knock knock knock, I'm the server")
+#print(client, port)
+client.send(b"Connection with Server Established")
 while True:
     message = input("What would you like to send?(or enter [close] if you want to stop messaging: ").encode("utf-8")
     if message.decode() == "[close]":
         break
     client.send(message)
     data = client.recv(buffer_size)
-    print("received data:")
+    print("received data:", data)
 mysocket.close()
