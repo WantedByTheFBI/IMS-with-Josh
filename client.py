cb7229225e6cdbb9fd39ecbfe7e1aea77dcbab3d
jackgood <jackmichaelgood@gmail.com>
2023-01-18 12:30:47 -0500

    threading working

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ def readincomingtext(name):
         data = s.recv(buffer_size)
         data = data.decode()
         print('\n')
-        logging.info("Received data %s:" + data, name)
+        logging.info("s% From Host: " + data, name)
 host = "Lab120-04"
 port = 9440
 buffer_size = 1024
@@ -14,14 +14,14 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 format = "%(asctime)s: %(message)s"
 logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
-x = threading.Thread(target=readincomingtext, args=(1,),daemon=True)
+x = threading.Thread(target=readincomingtext, args=("Message",),daemon=True)
 x.start()
+print("What would you like to send?(or enter [close] if you want to stop messaging: ")
+print("\n")
 while True:
-    text = input("What would you like to send?(or enter [close] if you want to stop messaging: ").encode("utf-8")
+    text = input().encode("utf-8")
     if text.decode() == "[close]":
         s.send(("The client has closed the application.").encode("utf-8"))
         break
     s.send(text)
-    data = s.recv(buffer_size)
-    print("received data:", data)
 s.close()
--- threading srever.py
+++ threading srever.py
@@ -1,12 +1,11 @@
 import logging
 import threading
 import socket
-def readincomingtext(name):
+def readincomingtext(name):  #the function that the thread will run until the code ends
     while True:
         data = client.recv(buffer_size)
         data = data.decode()
-        print('\n')
-        logging.info("Received data %s:" + data, name)
+        logging.info("s% From Client: " + data, name)
 
 mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 buffer_size = 1024
@@ -17,10 +16,12 @@ mysocket.listen()
 client.send(b"Connection with Server Established")
 format = "%(asctime)s: %(message)s"
 logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
-x = threading.Thread(target=readincomingtext, args=(1,),daemon=True)
+x = threading.Thread(target=readincomingtext, args=("Message",),daemon=True)
 x.start()
+print("What would you like to send?(or enter [close] if you want to stop messaging:")
+print("\n")
 while True:
-    message = input("What would you like to send?(or enter [close] if you want to stop messaging: ").encode("utf-8")
+    message = input().encode("utf-8")
     if message.decode() == "[close]":
         break
     client.send(message)
